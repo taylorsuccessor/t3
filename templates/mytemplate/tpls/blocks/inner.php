7d93ebaa99bf79decf0231e53f8eb919d3bdace0
taylorsuccessor <taylorsuccessor@gmail.com>
2016-05-01 15:27:40 +0300

    countModules > 1 not 0 for aside menu

--- templates/mytemplate/tpls/blocks/inner.php
+++ templates/mytemplate/tpls/blocks/inner.php
@@ -1,28 +1,17 @@
 
-<?php if ($this->countModules('innerAside')) : ?>
+<?php if ($this->countModules('innerAside')>1) : ?>
 
 <div class="row innerPagesContainer" >
     <div class="container ">
 
         <aside class="innerAside col-xs-3" >
-<!---->
-<!---->
-<!--            <a href="#" class=" button primaryButton ">APPLY FOR FREE DEMO</a>-->
-<!--            <a href="#" class=" button primaryButton "> APPLY FOR LIVE ACCOUNT</a>-->
-
                 <jdoc:include type="modules" name="<?php $this->_p('innerAside') ?>" style="raw" />
-
 
 </aside>
 
 
         <section class="innerPagesBody col-xs-9">
-
             <jdoc:include type="component" />
-
-
-
-
         </section>
 
 
